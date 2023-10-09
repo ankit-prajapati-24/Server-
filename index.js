da99d5a4b88d5492d5939981aa8bd7ceab9721f0
ankit-prajapati-24 <141832510+ankit-prajapati-24@users.noreply.github.com>
2023-10-09 19:37:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+
+const dbconnect = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const { ConnectCloadinary } = require("./config/Coudinary");
+const fileupload = require("express-fileupload");
+const dotenv = require("dotenv");
+dotenv.config(); // Load environment variables from .env file
+const PORT = process.env.PORT || 4000;
+
+// connect database
+dbconnect();
+
+// middleware
+const bodyparser = require("body-parser");
+
+app.use(cookieParser());
+app.use(bodyparser.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true
+  })
+);
+
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: "/tmp"
+  })
+);
+
+ConnectCloadinary();
+
+app.use("/api/v1/auth", userRoutes); // Add a leading slash to the routes
+// app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/profile", profileRoutes);
+// app.use("/api/v1/payment", paymentRoutes);
+
+// default route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Your server is running"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
